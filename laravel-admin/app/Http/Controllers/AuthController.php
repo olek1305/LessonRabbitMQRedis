a8602088936f6de401dee202d42446cd2183c946
Arcylisz <olek1305@gmail.com>
2024-09-02 18:55:21 +0200

    add RegisterRequest and function register

--- laravel-admin/app/Http/Controllers/AuthController.php
+++ laravel-admin/app/Http/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -34,4 +36,28 @@ class AuthController extends Controller
         // Failed login attempt
         return response()->json(['message' => 'Invalid Credentials!'], Response::HTTP_UNAUTHORIZED);
     }
+
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        // Creating a new user
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        // Generating a token using Passport
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->accessToken;
+
+        // Returning a response with a token
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+            'token_type' => 'Bearer',
+            'expires_at' => $tokenResult->token->expires_at->toDateTimeString(),
+        ], Response::HTTP_CREATED);
+    }
+
 }
