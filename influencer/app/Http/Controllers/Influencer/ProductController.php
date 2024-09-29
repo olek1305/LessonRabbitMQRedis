07f7f945b170a506527b0a2f6abcee59f2583fa4
Arcylisz <olek1305@gmail.com>
2024-09-29 19:35:44 +0200

    add filtering cached data

--- influencer/app/Http/Controllers/Influencer/ProductController.php
+++ influencer/app/Http/Controllers/Influencer/ProductController.php
@@ -5,21 +5,24 @@ namespace App\Http\Controllers\Influencer;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ProductController
 {
     public function index(Request $request)
     {
-        return \Cache::remember('products', 60*30, function() use($request) {
+        $products = \Cache::remember('products', 60*30, function() use($request) {
             sleep(2);
-            $query = Product::query();
 
-            if ($s = $request->input('s')) {
-                $query->whereRaw("title LIKE '%{$s}%'")
-                    ->orWhereRaw("description LIKE '%{$s}%'");
-            }
-
-            return ProductResource::collection($query->get());
+            return Product::all();
         });
+
+        if ($s = $request->input('s')) {
+            $products = $products->filter(function(Product $product) use($s) {
+                return Str::contains($product->title, $s) || Str::contains($product->description, $s);
+            });
+        }
+
+        return ProductResource::collection($products);
     }
 }
