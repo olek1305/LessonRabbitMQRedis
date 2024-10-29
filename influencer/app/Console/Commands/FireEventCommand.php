d3885832373b9c56260e7d7f2d67075d08b07769
Arcylisz <olek1305@gmail.com>
2024-10-29 09:17:02 +0100

    dispatching jobs

---
+++ influencer/app/Console/Commands/FireEventCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\AdminAdded;
+use Illuminate\Console\Command;
+
+class FireEventCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fire';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        AdminAdded::dispatch('example@gmail.com');
+    }
+}
---
+++ influencer/app/Jobs/AdminAdded.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class AdminAdded implements ShouldQueue
+{
+    use Queueable;
+
+    public $email;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //
+    }
+}
