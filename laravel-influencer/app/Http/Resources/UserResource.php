7b8808f309e5764113d3cbfc259f1470e739e201
Arcylisz <olek1305@gmail.com>
2024-11-15 22:22:37 +0100

    fix permissions

--- laravel-influencer/app/Http/Resources/UserResource.php
+++ laravel-influencer/app/Http/Resources/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -10,17 +11,20 @@ class UserResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @return array<string, mixed>
+     * @return array
      */
     public function toArray(Request $request): array
     {
+        $user = User::with('role.permissions')->find(auth()->id());
+
         return [
             'id' => $this->id,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'email' => $this->email,
-            'role' => $this->role?->name,
-            'permissions' => $this->permissions(),
+            $this->mergeWhen(\Auth::user() && \Auth::user()->isAdmin(), [
+                'role' => $this->role,
+            ]),
             $this->mergeWhen(\Auth::user() && \Auth::user()->isInfluencer(), [
                 'revenue' => $this->revenue,
             ]),
