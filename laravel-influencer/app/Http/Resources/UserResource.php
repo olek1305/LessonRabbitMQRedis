ef8fc34bb9650fa8a80c900aa00bb853dae8c38a
Arcylisz <olek1305@gmail.com>
2024-11-15 22:15:39 +0100

    fix

--- laravel-influencer/app/Http/Resources/UserResource.php
+++ laravel-influencer/app/Http/Resources/UserResource.php
@@ -19,9 +19,8 @@ class UserResource extends JsonResource
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'email' => $this->email,
-            $this->mergeWhen(\Auth::user() && \Auth::user()->isAdmin(), [
-                'role' => $this->role,
-            ]),
+            'role' => $this->role?->name,
+            'permissions' => $this->permissions(),
             $this->mergeWhen(\Auth::user() && \Auth::user()->isInfluencer(), [
                 'revenue' => $this->revenue,
             ]),
